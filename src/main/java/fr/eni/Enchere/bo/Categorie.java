7475408dd28bc6e7f1efbeb9fdc06d485fc1b382
Guillaume Cazorla <cazorla.guillaume@gmail.com>
2022-06-27 16:30:32 +0200

    creation de la class qu'il nous manquait

---
+++ src/main/java/fr/eni/Enchere/bo/Categorie.java
@@ -0,0 +1,42 @@
+package fr.eni.Enchere.bo;
+
+public class Categorie {
+	//attribue 
+private Integer noCategorie;
+private String libelle;
+
+//constructeur
+public Categorie(String libelle) {
+	this.libelle = libelle;
+}
+
+//get et set
+public Integer getNoCategorie() {
+	return noCategorie;
+}
+
+public void setNoCategorie(Integer noCategorie) {
+	this.noCategorie = noCategorie;
+}
+
+public String getLibelle() {
+	return libelle;
+}
+
+public void setLibelle(String libelle) {
+	this.libelle = libelle;
+}
+
+
+//to string 
+@Override
+public String toString() {
+	return "Categorie [noCategorie=" + noCategorie + ", libelle=" + libelle + "]";
+}
+	
+
+
+
+
+
+}
