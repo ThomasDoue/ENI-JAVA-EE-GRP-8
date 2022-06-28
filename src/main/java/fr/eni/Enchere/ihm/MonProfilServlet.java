378b09c7a0e05d9a7d493011b33c17eb99348913
xhouchard2022 <xhouchard2022@12SE14-HGMRZS2.ad.campus-eni.fr>
2022-06-28 12:25:13 +0200

    modification nom de classe MonProfilServley => monProfilServlet

--- src/main/java/fr/eni/Enchere/ihm/MonProfilServlet.java
+++ src/main/java/fr/eni/Enchere/ihm/MonProfilServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet({"","/RETOURFORMULAIRE"})
-public class MonProfilServley extends HttpServlet {
+public class MonProfilServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
