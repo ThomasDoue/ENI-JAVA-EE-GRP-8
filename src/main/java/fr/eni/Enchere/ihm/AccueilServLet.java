6ff24598b034384d025a79530012741143f06f60
ThomasDoue <thomasdoue49100@gmail.com>
2022-06-28 09:56:24 +0200

    Ajouts du Logo d'accueil

--- src/main/java/fr/eni/Enchere/ihm/AccueilServLet.java
+++ src/main/java/fr/eni/Enchere/ihm/AccueilServLet.java
@@ -9,14 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Accueil")
+@WebServlet({"", "/Accueil"})
 public class AccueilServLet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/pages/Accueil.jsp");
-		rd.forward(req, resp);
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/pages/Accueil.jsp").forward(req, resp);
 	}
 	
 
