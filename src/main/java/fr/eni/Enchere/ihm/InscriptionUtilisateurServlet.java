2a15b1f62f4eee78e79f5cd030d91f53b9df5178
Guillaume Cazorla <cazorla.guillaume@gmail.com>
2022-06-28 14:44:09 +0200

    mise a jour qui fonctionne

--- src/main/java/fr/eni/Enchere/ihm/InscriptionUtilisateurServlet.java
+++ src/main/java/fr/eni/Enchere/ihm/InscriptionUtilisateurServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet({"","/Inscription"})
+@WebServlet({"/Inscription"})
 public class InscriptionUtilisateurServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
--- src/main/webapp/WEB-INF/pages/monProfilGuigui.jsp
+++ src/main/webapp/WEB-INF/pages/monProfilGuigui.jsp
@@ -5,7 +5,8 @@
 <head>
 <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Insert title here</title>
+      <link rel="stylesheet" href="${pageContext.servletContext.contextPath}/css/style.css">
+<title>Retour Formulaire</title>
 </head>
 <body>
 
