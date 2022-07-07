4074d58f4689c51ffce042bd351d444f4458d3fd
Guillaume Cazorla <cazorla.guillaume@gmail.com>
2022-07-07 11:46:13 +0200

    modification de la fonction recuperation de l'adresse dans nouvelleVente

--- src/main/java/fr/eni/Enchere/bll/ArticleAVendreManager.java
+++ src/main/java/fr/eni/Enchere/bll/ArticleAVendreManager.java
@@ -1,8 +1,13 @@
 package fr.eni.Enchere.bll;
 
 import fr.eni.Enchere.bo.ArticleVendu;
+import fr.eni.Enchere.bo.Retrait;
 
 public interface ArticleAVendreManager {
-	int NouvelleArticle (ArticleVendu nouvelleArticle )throws BLLException,Exception;
+	int NouvelleArticle (ArticleVendu nouvelleArticle )throws BLLException;
+	
 	public boolean FinDEnchere (int noArticle) throws BLLException;
+	
+	void insertionDonnerRetrait (Retrait adresseRetrait) throws BLLException;
+	
 }
